2cb498c688f77fc36ce5fd253cf34b1e660d2637
MystiqueMide <176629124+mystiquemide@users.noreply.github.com>
2026-08-23 23:39:12 +0000

    Set outputFileTracingRoot and allowed dev origins

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  outputFileTracingRoot: __dirname,
+  allowedDevOrigins: ['*.trycloudflare.com', '*.ngrok-free.app', '*.ngrok-free.dev', '*.ngrok.app', '*.ngrok.io'],
 }
 
 module.exports = nextConfig
